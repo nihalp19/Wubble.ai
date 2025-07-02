354ac7f19e0fa3f2c0416a0dfe4b3dbbd2e2ef69
Nihal Panday <nihalpanday2020@gmail.com>
2025-07-02 14:09:42 +0530

    updated MagicCardDemo

--- frontend/src/demo-components/MagicCardDemo.tsx
+++ frontend/src/demo-components/MagicCardDemo.tsx
@@ -1,14 +1,27 @@
 "use client";
 import { NeonGradientCard } from "@/components/magicui/gradient-card";
+import { useSongStore } from "@/store/useSongStore";
 
 export default function MagicCardDemo({ title }: { title: string }) {
+  const { currentGenre, currentMood } = useSongStore();
+
+  const isSelected = currentGenre === title || currentMood === title;
+
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <NeonGradientCard
-        className="w-full h-[70px] text-base sm:text-xl text-center font-extrabold text-white transition-transform duration-300 transform hover:scale-105 flex items-center justify-center"
-      >
-        {title}
-      </NeonGradientCard>
+      {isSelected ? (
+        <NeonGradientCard
+          className="flex items-center justify-center w-full h-[55px] text-base sm:text-xl text-center font-extrabold text-white transition-transform duration-300 transform hover:scale-105"
+        >
+          {title}
+        </NeonGradientCard>
+      ) : (
+        <div
+          className="w-full h-[55px] text-base sm:text-xl text-center font-semibold text-white transition-transform duration-300 transform hover:scale-105 flex items-center justify-center border border-white/30 rounded-xl"
+        >
+          {title}
+        </div>
+      )}
     </div>
   );
 }
