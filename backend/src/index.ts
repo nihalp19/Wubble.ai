36a2b2f2acb62aee3f722629651857ac33194c98
Nihal Panday <nihalpanday2020@gmail.com>
2025-07-02 20:07:31 +0530

    fixed cors error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const FRONTEND_URL: string = process.env.FRONTEND_URL || 'http://localhost:5173';
 
 app.use(cors({
-  origin: [FRONTEND_URL],
+  origin: [FRONTEND_URL,"https://wubble-ai.vercel.app/"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
