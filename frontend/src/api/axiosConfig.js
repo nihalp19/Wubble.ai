f4ab2b186b6e367c8a0cc2923dda9372a3aad9ef
Nihal Panday <nihalpanday2020@gmail.com>
2025-07-02 14:08:56 +0530

    updated axiosconfig

--- frontend/src/api/axiosConfig.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
-  withCredentials: true, // Enable sending cookies with requests                                                                         
-});
