7e2fc526916ecf9329d3da210d97a144ccb31a24
Nihal Panday <nihalpanday2020@gmail.com>
2025-07-02 20:40:11 +0530

    fix cors errors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const FRONTEND_URL: string = process.env.FRONTEND_URL || 'http://localhost:5173';
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://wubble-ai.vercel.app/' // ✅ Replace with your actual frontend URL
+  'https://wubble-ai.vercel.app' // ✅ Replace with your actual frontend URL
 ];
 
 
